825924af6b3751100e65acb4e31be07d4ca10001
RaiDaVinci <raimondlica@gmail.com>
2024-10-07 12:43:09 +0200

    Add files via upload

---
+++ terzo.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def main():
+    mioArr=np.arange(3,100,2)
+    print(mioArr)
+    mioArr0=np.zeros((3,100))
+    print(mioArr0)
+    mioArr1=np.ones((4,2,3,10))
+    print(mioArr1)
+
+if __name__=="__main__":
+    main()
